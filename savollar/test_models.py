f81f4420daec89f62dfa2ba36961769f08ff8d45
muminoff <muminoff@naver.com>
2014-10-03 12:47:19 +0900

    100 models fetch

--- savollar/test_models.py
+++ savollar/test_models.py
@@ -6,8 +6,8 @@ from models import SavolModel
 
 def main():
     setup(settings["CASSANDRA_CLUSTER"], settings["CASSANDRA_KEYSPACE"])
-    print "Gettings first 10 models from DB:"
-    for model in SavolModel.objects.all()[:10]:
+    print "Gettings first 100 models from DB:"
+    for model in SavolModel.objects.all()[:100]:
         print model.savol_id
         print "---------------------"
         print model.title
